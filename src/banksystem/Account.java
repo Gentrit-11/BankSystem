cb979620790695d46fd88ba498501f3ed564a063
GentritSheremetaj <79097057+Gentrit-11@users.noreply.github.com>
2024-05-09 15:22:11 +0200

    publishing bank system project

---
+++ src/banksystem/Account.java
@@ -0,0 +1,50 @@
+package banksystem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Account {
+    private static int counter = 1;
+    private int accountId;
+    private String userName;
+    private double balance;
+    private List<Transaction> transactions;
+
+    public Account(String userName, double balance) {
+        this.accountId = counter++;
+        this.userName = userName;
+        this.balance = balance;
+        this.transactions = new ArrayList<>();
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        if (balance - amount < 0) {
+            throw new IllegalArgumentException("Account balance cannot go below zero!");
+        }
+        balance -= amount;
+    }
+
+    public void addTransaction(Transaction transaction) {
+        transactions.add(transaction);
+    }
+
+    public int getAccountId() {
+        return accountId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public List<Transaction> getTransactions() {
+        return transactions;
+    }
+}
---
+++ src/database/DatabaseConnection.java
@@ -0,0 +1,33 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/bank_system_db";
+    private static final String DB_USER = "root";
+    private static final String DB_PASSWORD = "";
+    private static Connection connection = null;
+
+    public static Connection getConnection() {
+        try {
+            if (connection == null || connection.isClosed()) {
+                connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    public static void closeConnection() {
+        try {
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
